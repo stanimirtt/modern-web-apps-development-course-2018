b16a6d14090494308614259491f4c13a72fd5a60
Stanimir Todorov <stodorov@softserveinc.com>
2018-07-20 21:18:19 +0300

    Small fix.

--- 03 React With MobX/src/pages/Home.jsx
+++ 03 React With MobX/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { observer, inject } from 'mobx-react';
 import { SearchBox, ItemList, ItemDetail } from '../components';
 import { appStatePropType, appStatePropTypeDefaults } from '../stores';
 
+// TODO: Convert to dummy
 @inject('appState')
 @observer
 class Home extends React.Component {
