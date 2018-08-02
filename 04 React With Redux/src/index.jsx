03c01407ef284425b892b4dbac4f02a4ec066c17
Stanimir Todorov <stodorov@softserveinc.com>
2018-08-02 20:55:08 +0300

    Add devtools.

--- 04 React With Redux/src/index.jsx
+++ 04 React With Redux/src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+  reducers,
+  /* preloadedState, */ devToolsEnhancer()
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('.container')
